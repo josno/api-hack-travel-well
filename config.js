aec1c98e2c0b37e437d336bcb08bcf62b57b05a1
Joanne Senoren <jnsenoren@gmail.com>
2019-10-22 23:08:32 +0400

    removed config

--- config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-  coordkey: 'be3fdc44a8fcb1f93b5219a841e601cf',
-  weatherkey: 'dc87cbf11257ad136a53a6eb41f28e06',
-  currencyKey: 'ce2cecd71a30de95b210',
-  visaKey: 'AIzaSyCvrk8ZdgVVsToO93v0v5nMheFb5-AaUjk',
-  googleKey: 'AIzaSyAaNlFg2aeB_By2qvD6eiDCS3pJYNsswpI'
-};
